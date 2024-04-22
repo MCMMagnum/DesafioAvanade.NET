8a6ce21518f39b54708b0e8c3421826141ef87e9
Magnum Souza <fera.magnum.souza@gmail.com>
2024-04-22 16:55:39 -0300

    primeiroCommit

---
+++ DesafioDeCodigo/Models/Reserva.cs
@@ -0,0 +1,58 @@
+using DesafioDeCodigo.Models;
+
+namespace DesafioDeCodigo.Models
+{
+    public class Reserva
+    {
+        public List<Pessoa> Hospedes { get; set; }
+        public Suite Suite { get; set; }
+        public int DiasReservados { get; set; }
+
+        public Reserva() { }
+
+        public Reserva(int diasReservados)
+        {
+            DiasReservados = diasReservados;
+        }
+
+        public void CadastrarHospedes(List<Pessoa> hospedes)
+        {
+
+            if (Suite.Capacidade >= hospedes.Count())
+            {
+                Hospedes = hospedes;
+            }
+            else
+            {
+
+                throw new ArgumentException("Capacidade de suites excedida - Lotado!");
+            }
+        }
+
+        public void CadastrarSuite(Suite suite)
+        {
+            Suite = suite;
+        }
+
+        public int ObterQuantidadeHospedes()
+        {
+
+            return Hospedes.Count();
+
+        }
+
+        public decimal CalcularValorDiaria()
+        {
+
+            decimal valor = DiasReservados * Suite.ValorDiaria;
+
+
+            if (DiasReservados >= 10)
+            {
+                valor = valor - ((valor/100)*10);
+            }
+
+            return valor;
+        }
+    }
+}
